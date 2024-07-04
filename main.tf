c929a05a6a10ee759d922232a055dffae45de320
jaspal2 <47457335+jaspal2@users.noreply.github.com>
2024-07-04 15:08:20 +1000

    network interface

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ data "aws_ami" "app_ami" {
 
 resource "aws_vpc" "vpc_custom" {
   cidr_block  =     "10.0.0.0/24"
-  default     =     true
+ 
 }
 
 
@@ -36,10 +36,25 @@ resource "aws_subnet" "subnet_custom2" {
   cidr_block =    "10.0.0.128/25"
 }
 
+
+resource "aws_network_interface" "custom_network_interface" {
+  subnet_id   = aws_subnet.subnet_custom1.id
+  private_ips = ["10.0.0.5"]
+
+  tags = {
+    Name = "custom_network_interface"
+  }
+}
+
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
   vpc_security_group_ids = [aws_security_group.terraform_SG.id]
+
+  network_interface {
+    network_interface_id = aws_network_interface.custom_network_interface.id
+    device_index         = 0
+  }
   
   tags = {
     Name = "Hello World"

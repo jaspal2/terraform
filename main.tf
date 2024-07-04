542734cdc5e8d0c3936a014ef6622f7347db39e0
jaspal2 <47457335+jaspal2@users.noreply.github.com>
2024-07-04 11:58:41 +1000

    SG changes

--- main.tf
+++ main.tf
@@ -11,14 +11,50 @@ data "aws_ami" "app_ami" {
     values = ["hvm"]
   }
 
+  data "aws_vpc" "default" {
+    default = true
+  }
+
   owners = ["979382823631"] # Bitnami
 }
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
+  vpc_security_group_ids = [aws_security_group.terraform_SG.id]
+
+  
 
   tags = {
     Name = "Hello World"
   }
 }
+
+
+
+resource "aws_security_group" "terraform_SG"{
+  name =   "terraform_SG"
+  vpc_id  =  data.aws_vpc.default.id
+}
+
+resource "aws_security_group_rule" "terraform_SG_ingress_https" {
+  type              = "ingress"
+  description       = "Security group for Ingress request"
+  from_port         = 0
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = [0.0.0.0/0]
+  security_group_id = aws.aws_security_group.id
+}
+
+
+resource "aws_security_group_rule" "terraform_SG_egress_https" {
+  type              = "egress"
+  description       = "Security group for egress request"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = [0.0.0.0/0]
+  security_group_id = aws.aws_security_group.id
+}
+

a8a1144f9d17384191f64978a612fda1976aceb3
jaspal2 <47457335+jaspal2@users.noreply.github.com>
2024-07-04 15:17:12 +1000

    correct vpc

--- main.tf
+++ main.tf
@@ -37,24 +37,11 @@ resource "aws_subnet" "subnet_custom2" {
 }
 
 
-resource "aws_network_interface" "custom_network_interface" {
-  subnet_id   = aws_subnet.subnet_custom1.id
-  private_ips = ["10.0.0.5"]
-
-  tags = {
-    Name = "custom_network_interface"
-  }
-}
-
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
   vpc_security_group_ids = [aws_security_group.terraform_SG.id]
-
-  network_interface {
-    network_interface_id = aws_network_interface.custom_network_interface.id
-    device_index         = 0
-  }
+  subnet_id   =    aws_subnet.subnet_custom1.id
   
   tags = {
     Name = "Hello World"

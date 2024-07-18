7195f74111d3b531e4ebb917be27be51d3748ec4
jaspal2 <47457335+jaspal2@users.noreply.github.com>
2024-07-18 10:39:59 +1000

    change ami to variable value

--- variables.tf
+++ variables.tf
@@ -2,3 +2,12 @@ variable "instance_type" {
   description = "Type of EC2 instance to provision"
   default     = "t3.nano"
 }
+
+
+variable "ami_name" {
+    description      =      "AMI name for an instance"
+    default       =      "bitnami-tomcat-*-x86_64-hvm-ebs-nami"
+}
+
+
+
